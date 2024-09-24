72996d9ce23015e9e33a65419fc22ff237744a82
Walker-Gideon <gideonawortwe3@gmail.com>
2024-09-24 20:19:58 +0000

    written my the javascript for the date in the header

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const todayDay = document.querySelector(".date-day");
+const todayDate = document.querySelector(".date");
+
+//? Dashboard Date
+const date = new Date();
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const dayName = daysOfWeek[date.getDay()];
+todayDay.textContent = dayName;
+
+const day = date.getDate();
+const month = (date.getMonth() + 1).toString();
+const year = date.getFullYear();
+todayDate.textContent = `${day}/${month.padStart(2, "0")}/${year}`;
