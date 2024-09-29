c819710ab1c64d7aa32bb95202f40b9a5cc28efd
Walker-Gideon <gideonawortwe3@gmail.com>
2024-09-29 04:40:40 +0000

    Working on the task status bar

--- script.js
+++ script.js
@@ -24,3 +24,14 @@ todayDay.textContent = dayName;
 // todayDate.textContent = `${day}/${month.padStart(2, "0")}/${year}`;
 
 todayDate.textContent = date.toLocaleDateString();
+
+//? PROGRESS on TASK Container
+document.querySelectorAll(".circle-progress").forEach((circle) => {
+  const progress = circle.getAttribute("data-progress");
+  const circumference = 2 * Math.PI * 40; // 2πr, where r is 40 (radius of circle)
+
+  // Update the circle stroke based on progress
+  const progressCircle = circle.querySelector(".progress");
+  const offset = circumference - (progress / 100) * circumference;
+  progressCircle.style.strokeDashoffset = offset;
+});
